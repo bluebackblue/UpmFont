eea961fb73a580991ecbfed579cb44610af4875a
blueback <blueback28@gmail.com>
2022-08-15 21:22:08 +0900

    更新 : VersionManager

--- BlueBackFont/Assets/UPM/Runtime/BlueBack/Font/Version.cs
+++ BlueBackFont/Assets/UPM/Runtime/BlueBack/Font/Version.cs
@@ -21,7 +21,7 @@ namespace BlueBack.Font
 
 		/** packageversion
 		*/
-		public const string packageversion = "0.0.16";
+		public const string packageversion = "0.0.17";
 
 		/** GetPackageVersion
 		*/
